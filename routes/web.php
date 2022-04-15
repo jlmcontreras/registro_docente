485910b29a090f4c008fcf45347b9b51ad5b6719
jlmcontreras <joseluismauriciocontreras@gmail.com>
2022-04-15 00:00:29 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocenteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/layouts/home', function () {
+    return view('layouts.home');
+})->middleware(['auth'])->name('home');
+
+
+
+Route::get('docente/index', [DocenteController::class, 'index'])->name('docente.index')->middleware(['auth']);
+
+Route::get('docente/create', [DocenteController::class,'create'])->name('docente.create')->middleware(['auth']);
+Route::post('docente/store',[ DocenteController::class,'store'])->name('docente.store')->middleware(['auth']);
+Route::get('docente/{id}/show', [DocenteController::class,'show'])->name('docente.show')->middleware(['auth']);
+Route::delete('docente/{id}/destroy', [DocenteController::class,'destroy'])->name('docente.destroy')->middleware(['auth']);
+Route::get('docente/{id}/confirm', [DocenteController::class,'confirm'])->name('docente.confirm')->middleware(['auth']);
+Route::get('docente/{id}/edit', [DocenteController::class,'edit'])->name('docente.edit')->middleware(['auth']);
+Route::put('docente/{id}/update', [DocenteController::class,'update'])->name('docente.update')->middleware(['auth']);
+
 
 require __DIR__.'/auth.php';
