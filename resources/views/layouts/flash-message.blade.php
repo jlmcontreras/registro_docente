c7a69af3892cd46598dd46a4fdc8a7ffab1b5aea
jlmcontreras <joseluismauriciocontreras@gmail.com>
2022-04-25 11:38:23 -0300

    Update flash-message.blade.php

--- resources/views/layouts/flash-message.blade.php
+++ resources/views/layouts/flash-message.blade.php
@@ -1,44 +1,35 @@
-<<<<<<< Updated upstream
-<?php
 
-@if ($message = Session::get('success'))
-=======
 @extends('layouts.home')
 @section('flash')
 
 @if ($message = session()->get('success'))
->>>>>>> Stashed changes
+
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
-<<<<<<< Updated upstream
-@if ($message = Session::get('error'))
-=======
+
 @if ($message = session()->get('error'))
->>>>>>> Stashed changes
     <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
-<<<<<<< Updated upstream
-@if ($message = Session::get('warning'))
-=======
+
+
 @if ($message = session()->get('warning'))
->>>>>>> Stashed changes
+
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>{{ $message }}</strong>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
 
-<<<<<<< Updated upstream
-@if ($message = Session::get('info'))
-=======
+
+
 @if ($message = session()->get('info'))
 >>>>>>> Stashed changes
     <div class="alert alert-info alert-dismissible fade show" role="alert">
@@ -53,8 +44,9 @@
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
-<<<<<<< Updated upstream
-=======
+
 
 @endsection
->>>>>>> Stashed changes
+
+
+
