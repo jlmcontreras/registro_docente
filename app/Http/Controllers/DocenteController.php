48f541834c158bd0dfd44255cf9f58749a185071
jlmcontreras <joseluismauriciocontreras@gmail.com>
2022-06-21 21:23:57 -0300

    Update DocenteController.php

--- app/Http/Controllers/DocenteController.php
+++ app/Http/Controllers/DocenteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Docente;
 use App\Models\Persona;
 
+use http\Env\Response;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -22,6 +23,12 @@ class DocenteController extends Controller
         //  return view('docentes.docente_index2', compact('docentes'));
         return view('docentes.docente_index', compact('docentes'));
 
+    }
+
+    public function search(Request  $request)
+    {
+        $data= index('docentes')->search('$request.search')::paginate(10);
+        return view('docentes.docente_index', compact('data'));
     }
 
     /**
@@ -119,11 +126,11 @@ class DocenteController extends Controller
     }
 
 
-    public function show(Docente $docente)
+    public function show($id)
     {
         //
         $docente = Docente::find($id);
-        return view('docente.docente_show', compact('docente'));
+        return view('docentes.docente_show', compact('docente'));
     }
 
     /**
@@ -149,9 +156,57 @@ class DocenteController extends Controller
      * @param  \App\Models\Docente  $docente
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Docente $docente)
+    public function update(Request $request, $id)
     {
         //
+        $request->validate([
+            'documento' => 'required', 'string', 'max:24',
+            'apellido' => 'required|string|max:45',
+            'nombres' => 'required|string|max:75',
+            'sexo' => ['required', Rule::in('M', 'F')],
+            'fecha_nacimiento' => 'required|date',
+            'domicilio' => 'required|string|max:255',
+            'localidad' => 'required|string|max:45',
+            'departamento' => 'required|string|max:45',
+            'telefono' => 'nullable|string|max:45',
+            'celular' => 'required|string|max:45',
+            'correo' => 'nullable|email|max:75',
+            'titulo' => 'required|string|max:45',
+        ]);
+        try {
+            DB::beginTransaction();
+            $docente = Docente::find($id);
+
+            $persona = Persona::find($docente->persona_id);
+            $persona->apellido = $request->get('apellido');
+            $persona->nombres = $request->get('nombres');
+            $persona->sexo = $request->get('sexo');
+            $persona->fecha_nacimiento = $request->get('fecha_nacimiento');
+            $persona->domicilio = $request->get('domicilio');
+            $persona->localidad = $request->get('localidad');
+            $persona->departamento = $request->get('departamento');
+            $persona->telefono = $request->get('telefono');
+            $persona->celular = $request->get('celular');
+            $persona->correo = $request->get('correo');
+
+            $persona->update();
+
+
+            $docente->titulo = $request->get('titulo');
+            $docente->update();
+
+            DB::commit();
+            return redirect()->route('docente.index')
+                ->with('success', "Docente Actualizado Correctamente");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            if ($e->getCode() == 23000) {
+                return redirect()->route('docente.index')
+                    ->with('danger', "Los Datos no han sido Actualizados");
+            }
+            return redirect()->route('docente.index')
+                ->with('danger', "Los Datos no han sido Actualizados" . $e->getMessage());
+        }
     }
 
     /**
